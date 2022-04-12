700ea61daf240ed8cc1d6a24f6cffeb97665998d
bryce0516 <jayhspark@gmail.com>
2022-04-12 23:40:42 +0900

    update

---
+++ CalculateBrain.swift
@@ -0,0 +1,13 @@
+//
+//  CalculateBrain.swift
+//  BMI Calculator
+//
+//  Created by Hye Sung Park on 2022/04/12.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct CalculateBrain {
+    <#fields#>
+}
