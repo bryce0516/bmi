0b53c8d45008d78ed39e4e840d99f63cd8f5b093
bryce0516 <jayhspark@gmail.com>
2022-04-12 23:57:11 +0900

    update

--- BMI Calculator/Controllers/CalculateViewController.swift
+++ BMI Calculator/Controllers/CalculateViewController.swift
@@ -22,6 +22,7 @@ class CalculateViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
     @IBAction func heightSliderChanged(_ sender: UISlider) {
--- CalculateBrain.swift
+++ CalculateBrain.swift
@@ -10,10 +10,10 @@ import Foundation
 
 struct CalculateBrain {
     
-    var bmi: Float = 0.0
+    var bmi: Float?
     
     func getBMIValue() -> String {
-        let bmiTo1DecimalPlace = String(format: "%0.1f", bmi)
+        let bmiTo1DecimalPlace = String(format: "%0.1f", bmi!)
         return bmiTo1DecimalPlace
     }
     
